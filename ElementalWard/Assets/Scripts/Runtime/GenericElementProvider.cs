1774990800feed71a6f0e55bd561db05e4c56840
Nebby <nebby131999@gmail.com>
2023-05-12 14:31:00 -0400

    Started scripts, added SimpleAddresser

---
+++ ElementalWard/Assets/Scripts/Runtime/GenericElementProvider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace ElementalWard
+{
+    public interface IElementProvider
+    {
+
+    }
+    public class GenericElementProvider : MonoBehaviour, IElementProvider
+    {
+
+    }
+}
