7227972b3509174ae9e58678a5a723ca2272ab9b
Nebby <nebby131999@gmail.com>
2023-11-06 22:05:30 -0300

    Fixed wandering soul file

--- ElementalWard/Assets/Scripts/Runtime/EntityStates/WanderingSoul/FireBone.cs
+++ ElementalWard/Assets/Scripts/Runtime/EntityStates/WanderingSoul/FireBone.cs
@@ -6,42 +6,76 @@ namespace EntityStates.WanderingSoul.Weapon
 {
     public class FireBone : BaseCharacterState
     {
-        public static GameObject projectilePrefab;
         public static float baseDuration;
         public static string animationName;
-        public static string playbackRateParam;
+
+        [Header("Projectile Settings")]
+        public static GameObject projectilePrefab;
         public static float movementSpeed;
         public static float projectileLifetime;
         public static float damageCoefficient;
 
-        private float duration;
-        private Ray aimRay;
+        private float _duration;
+        private bool _hasFired;
+        private CharacterAnimationEvents _characterAnimEvents;
+        private Ray _aimRay;
         public override void OnEnter()
         {
             base.OnEnter();
-            duration = baseDuration / 2;
-            PlayAnimation("Base", animationName, "attackSpeed", duration);
+            _duration = baseDuration / attackSpeedStat;
+
+            _characterAnimEvents = GetAnimationEvents();
+            if(_characterAnimEvents)
+                _characterAnimEvents.OnAnimationEvent += FireAttack;
+
+            PlayAnimation("Base", animationName, "attackSpeed", _duration);
         }
+
+        private void FireAttack(int obj)
+        {
+            if(obj == CharacterAnimationEvents.fireAttackHash && !_hasFired)
+            {
+                _hasFired = true;
+                Fire();
+            }
+        }
+
         public override void FixedUpdate()
         {
             base.FixedUpdate();
-            if (FixedAge > duration)
+            if (FixedAge > _duration)
             {
-                aimRay = GetAimRay();
-                FireProjectileInfo projectileInfo = new FireProjectileInfo()
+                if (!_hasFired)
                 {
-                    instantiationPosition = aimRay.origin,
-                    instantiationRotation = Quaternion.LookRotation(aimRay.direction, Vector3.up),
-                    damageType = DamageType.None,
-                    owner = new BodyInfo(GameObject),
-                };
-                projectileInfo.AddProperty(CommonProjectileProperties.MovementSpeed, movementSpeed);
-                projectileInfo.AddProperty(CommonProjectileProperties.ProjectileLifeTime, projectileLifetime);
-                projectileInfo.AddProperty(CommonProjectileProperties.DamageCoefficient, damageCoefficient);
-
-                ProjectileManager.SpawnProjectile(projectilePrefab, projectileInfo);
+                    _hasFired = true;
+                    Fire();
+                }
                 outer.SetNextStateToMain();
             }
+        }
+
+        private void Fire()
+        {
+            _aimRay = GetAimRay();
+            FireProjectileInfo projectileInfo = new FireProjectileInfo()
+            {
+                instantiationPosition = _aimRay.origin,
+                instantiationRotation = Quaternion.LookRotation(_aimRay.direction, Vector3.up),
+                damageType = DamageType.None,
+                owner = new BodyInfo(GameObject),
+            };
+            projectileInfo.AddProperty(CommonProjectileProperties.MovementSpeed, movementSpeed);
+            projectileInfo.AddProperty(CommonProjectileProperties.ProjectileLifeTime, projectileLifetime);
+            projectileInfo.AddProperty(CommonProjectileProperties.DamageCoefficient, damageCoefficient);
+
+            ProjectileManager.SpawnProjectile(projectilePrefab, projectileInfo);
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            if (_characterAnimEvents)
+                _characterAnimEvents.OnAnimationEvent -= FireAttack;
         }
 
         public override InterruptPriority GetMinimumInterruptPriority()
